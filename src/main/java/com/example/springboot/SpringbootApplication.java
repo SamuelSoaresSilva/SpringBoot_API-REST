cddc41332f04895f08ed1c08c6e1d1b5cb1ee337
Jhean Victo Barbosa Almeida <jheanbarbosa1233@gmail.com>
2023-10-10 11:10:39 -0300

    Documentação da API feita com Swagger-UI

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,9 +1,12 @@
 package com.example.springboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API noNick", version = "1", description = "API de uma loja de roupa de grife"))
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.example.springboot.controllers;
 import com.example.springboot.dtos.ProductRecordDto;
 import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,19 +20,23 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@Tag(name =  "Products")
+@RequestMapping(value = "/products")
 @CrossOrigin(origins = "http://127.0.0.1:5500")
 //Rest Controller é uma derivada da @Controller, mas direcionada a uma api Restful
 public class ProductController {
 
+    /*
+     URL Local: http://localhost:8080/swagger-ui/index.html#/
+     */
 
-    //Endereço da pasta de imagens
-    //private static String caminhoImagens = "Endereço";
 
     @Autowired
     ProductRepository productRepository;
     //Logo após, criei um DTO Data Transfer Object
 
-    @PostMapping({"/products", "/products/"})
+    @Operation(summary = "Add um produto no banco")
+    @PostMapping
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
@@ -38,7 +44,8 @@ public class ProductController {
         //Metodo Post pronto porem sem customização de mensagem de erro ou de exceptions
     }
 
-    @GetMapping({"/products", "/products/"})
+    @Operation(summary = "Busca todos os produtos presente no banco")
+    @GetMapping()
     public ResponseEntity<List<ProductModel>> getAllProducts(){
         List<ProductModel> productsList = productRepository.findAll();
         if (!productsList.isEmpty()){
@@ -53,7 +60,8 @@ public class ProductController {
         }
     }
 
-    @GetMapping("/products/{id}")
+    @Operation(summary = "Busca um produto no banco pelo ID")
+    @GetMapping("/{id}")
     public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id){
         Optional<ProductModel> productO = productRepository.findById(id);
         if (productO.isEmpty()){
@@ -63,7 +71,8 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
 
-    @PutMapping("/products/{id}")
+    @Operation(summary = "Atualiza um produto no banco pelo ID")
+    @PutMapping("/{id}")
     public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
                                                 @RequestBody @Valid ProductRecordDto productRecordDto) {
         Optional<ProductModel> productO = productRepository.findById(id);
@@ -75,7 +84,8 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
     }
 
-    @DeleteMapping("/products/{id}")
+    @Operation(summary = "Deleta um produto no banco pelo ID")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id){
         Optional<ProductModel> productO = productRepository.findById(id);
         if (productO.isEmpty()){
@@ -85,7 +95,8 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body("Product has been deleted");
     }
 
-    @DeleteMapping({"/products", "/products/"})
+    @Operation(summary = "Deleta todos os produtos do banco")
+    @DeleteMapping()
     public ResponseEntity<Object> deleteAllProducts(){
         List<ProductModel> productsList = productRepository.findAll();
         if (productsList.isEmpty()){
