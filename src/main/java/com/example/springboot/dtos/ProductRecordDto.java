c002daf2413b0e720cc0c1461e031a7557a3563d
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2023-11-01 14:36:45 -0300

    Commit_6.2 Adding validation to the product image field

--- src/main/java/com/example/springboot/dtos/ProductRecordDto.java
+++ src/main/java/com/example/springboot/dtos/ProductRecordDto.java
@@ -2,6 +2,7 @@ package com.example.springboot.dtos;
 //Impede que os valores sejam nulos
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.URL;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
