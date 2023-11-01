726539ad46b4fe94df282d95a492be0895d9402c
SamuelSoaresSilva <samuel.soares.cunha64@gmail.com>
2023-11-01 14:33:17 -0300

    Commit_6.2 Simplifying in-product image storage

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -6,7 +6,7 @@ import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.Set;
+import java.util.HashSet;
 import java.util.UUID;
 @Data
 @Entity
